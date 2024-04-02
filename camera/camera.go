bc1696d86af84e357d2b0d3639e837e7cb02b029
BroMandarin <bromandarin@gmail.com>
2024-04-02 16:53:44 +0300

    feat: add camera.script and camera.go

---
+++ camera/camera.go
@@ -0,0 +1,38 @@
+components {
+  id: "script"
+  component: "/camera/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.777778\n"
+  "fov: 3.14\n"
+  "near_z: -1.0\n"
+  "far_z: 1.0\n"
+  "auto_aspect_ratio: 0\n"
+  "orthographic_projection: 1\n"
+  "orthographic_zoom: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
